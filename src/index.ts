48301f6f3abb334361f3ebb2bff42bd3f62e3885
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2023-11-13 00:20:17 +1100

    Add oauth2.0 flow stuff to get user token instead of client credential token

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,28 @@
 import fs from 'fs';
+import express from 'express';
+const server = express();
 import {getEnvVar} from "./env";
 import {getPlaylistTracks, populateTrackFeatures} from "./spotify";
+
+const getServerPort = () => {
+    if (process.env.PORT) {
+        return parseInt(process.env.PORT);
+    }
+    return 3000;
+}
 export class TokenCache {
     static token: string;
     static expires_at: number;
 }
 export const fetchAccessToken = async () => {
     if (!TokenCache.token || Date.now() > TokenCache.expires_at) {
-        const body = new URLSearchParams({
-            grant_type: "client_credentials",
-            client_id: getEnvVar("CLIENT_ID"),
-            client_secret: getEnvVar("CLIENT_SECRET"),
-        });
-        const res = await fetch("https://accounts.spotify.com/api/token", {
-            method: "POST",
-            headers: [["Content-Type", "application/x-www-form-urlencoded"]],
-            body
-        })
-        const json = await res.json();
-        TokenCache.token = json.access_token;
-        TokenCache.expires_at = Date.now() + json.expires_in * 1000;
+        throw new Error("Access Token is undefined or expired");
     }
     return TokenCache.token;
 }
 
-const start = async () => {
+//This will be run once our auth flow is complete
+const run = async () => {
     const playlistTracks = await getPlaylistTracks();
     const populated = await populateTrackFeatures(playlistTracks);
     const goodSongs = [];
@@ -37,6 +35,46 @@ const start = async () => {
     console.log(`Selected ${goodSongs.length} tracks, writing them to goodsongs.json`);
     fs.writeFileSync('./goodsongs.json', JSON.stringify(goodSongs, null, 2));
     console.log (`done :)`);
+
 }
 
-start();
+//OAuth 2.0 server stuff below here
+
+server.get('/', async (req, res) => {
+    const {code} = req.query as any;
+    const body = new URLSearchParams({
+        code,
+        redirect_uri: `http://localhost:${getServerPort()}/`,
+        grant_type: 'authorization_code'
+    });
+    const response = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: [
+            ["content-type", "application/x-www-form-urlencoded"],
+            ["Authorization", "Basic " + Buffer.from(getEnvVar("CLIENT_ID") + ":" + getEnvVar("CLIENT_SECRET")).toString('base64')],
+        ],
+        body
+    });
+    const json = await response.json();
+    if (response.status !== 200) {
+        res.status(500).send("Error fetching an access token");
+        throw new Error(json);
+    }
+    console.log(json);
+    TokenCache.token = json.access_token;
+    TokenCache.expires_at = Date.now() + json.expires_at;
+    res.status(200).send("Saved access token, you may now close this tab :)");
+    //BEGIN
+    run();
+})
+
+server.listen(getServerPort(), () => {
+    const authParams = new URLSearchParams({
+        response_type: 'code',
+        client_id: getEnvVar("CLIENT_ID"),
+        scope: "playlist-read-collaborative playlist-modify-public playlist-modify-private",
+        redirect_uri: `http://localhost:${getServerPort()}/`,
+    })
+
+    console.log(`https://accounts.spotify.com/authorize?${authParams.toString()}`);
+});
